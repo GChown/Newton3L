b103fdf99d83c4509dc60a252c10d9f151454eb1
Gordie Chown <gordie.chown@gmail.com>
2016-11-09 15:14:19 -0500

    Update textures, add reload time

---
+++ Assets/Dialogue/Speak.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Speak : MonoBehaviour {
+    public float speed; //characters per second
+    private string text;
+    private int currentChar;
+    public Text textBox;
+    public string words;
+    public RawImage Pic;
+    public GameObject child;
+    public GameObject emitter;
+	// Use this for initialization
+	void Start () {
+        talk(words);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 position = new Vector3(emitter.transform.position.x, emitter.transform.position.y, emitter.transform.position.z);
+        position = Camera.main.WorldToScreenPoint(position);
+        child.transform.position = position;
+        if (Input.GetKeyDown(KeyCode.Return) && child.activeSelf && currentChar < text.Length){
+            textBox.text = text;
+            currentChar = text.Length;
+        }
+	}
+
+    public void talk(string text)/*,Texture pic)*/{
+        child.SetActive(true);
+        this.text = text;
+        currentChar = 0;
+        textBox.text = "";
+        StartCoroutine(stepText());
+        //Pic.texture = pic;
+    }
+
+    IEnumerator stepText() {
+        if(text.Length > currentChar){
+            textBox.text += text[currentChar++];
+            yield return new WaitForSeconds(1/speed);
+            StartCoroutine(stepText());
+        } else {
+            yield return new WaitForSeconds(10);
+            child.SetActive(false);
+        }
+    }
+}
--- Assets/ShipController.cs
+++ Assets/ShipController.cs
@@ -3,13 +3,16 @@
 public class ShipController : MonoBehaviour {
     public GameObject _bulletEmitter;
     public GameObject _bulletPrefab;
+    float timeToShoot;
 
     // Use this for initialization
     void Start () {
+        timeToShoot = 0f;
     }
 
     // Update is called once per frame
     void Update () {
+        timeToShoot -= 1f;
         Vector3 mouse_pos = Input.mousePosition;
         mouse_pos.z = 5.23f;
         //The distance between the camera and object
@@ -22,14 +25,19 @@ public class ShipController : MonoBehaviour {
         Quaternion bulletRotation = Quaternion.Euler (ShipRotation);
         transform.rotation = bulletRotation;
         Debug.DrawRay(transform.position, ShipRotation, Color.green);
-        if(Input.GetKeyDown(KeyCode.Space)){
-            //Shoot a bullet
-            var bullet = (GameObject) Instantiate(_bulletPrefab, _bulletEmitter.transform.position, transform.rotation);
-            Rigidbody2D body = bullet.GetComponent<Rigidbody2D>();
-            Rigidbody2D shipRigid = GetComponent<Rigidbody2D>();
-            var force = mouse_pos.normalized * 20;
-            body.AddForce(force);
-            shipRigid.AddForce(-force);
+        if(Input.GetKey(KeyCode.Space)){
+            if(timeToShoot <= 0f){
+                //Shoot a bullet
+                var bullet = (GameObject) Instantiate(_bulletPrefab, _bulletEmitter.transform.position, transform.rotation);
+                Rigidbody2D body = bullet.GetComponent<Rigidbody2D>();
+                var force = mouse_pos.normalized * 20;
+                body.AddForce(force);
+                Rigidbody2D shipRigid = GetComponent<Rigidbody2D>();
+                shipRigid.AddForce(-force);
+                timeToShoot = 100f;
+            }else{
+                //Misfire animation?
+            }
         }
     }
 }
