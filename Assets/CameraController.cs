4e2e2e7124a00ca2981f0a79633c59209162e2d6
Gordie Chown <gordie.chown@gmail.com>
2016-11-30 21:03:36 -0500

    Added proper background scrolling

---
+++ Assets/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+    public GameObject _target;
+
+	// Use this for initialization
+	void Start () {
+        transform.transform.position = new Vector3(_target.transform.position.x, _target.transform.position.y, transform.transform.position.z);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        transform.transform.position = new Vector3(_target.transform.position.x, _target.transform.position.y, transform.transform.position.z);
+	}
+}
---
+++ Assets/ScrollBackground.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollBackground : MonoBehaviour {
+    public GameObject _target;
+
+    // Use this for initialization
+    void Start () {
+        Vector2 offset = _target.transform.position.normalized;
+        GetComponent<Renderer>().material.mainTextureOffset = offset;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Vector2 targetPos = _target.transform.position;
+        Vector2 offset = Camera.main.ScreenToWorldPoint(targetPos).normalized;
+        GetComponent<Renderer>().material.mainTextureOffset = offset;
+        transform.position = new Vector3(targetPos.x, targetPos.y, transform.position.z);
+    }
+}
